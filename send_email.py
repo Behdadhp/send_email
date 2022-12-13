c60a129824ea36f758080c3cb0a3d136b15df785
behdad hajipour <behdad.hajipour95@gmail.com>
2022-12-13 17:12:21 +0100

    edited send_emaild.py

--- send_email.py
+++ send_email.py
@@ -1,19 +1,18 @@
 import smtplib, ssl
 
-with open("test.txt") as f:
+with open("test_email.txt") as f:
     lines = f.readline()
-
 test_email = lines.split(", ")
 
 with open("context.txt") as f:
     context = f.read()
+
 
 port = 465 
 smtp_server = "smtp.gmail.com"
 sender_email = "your email address"
 password = "generate your own app password"
 message = context.encode("utf-8")
-
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
