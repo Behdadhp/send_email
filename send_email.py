f309ffe0d28971fb659b4ccb9df2b26dfaaa9efb
behdad hajipour <behdad.hajipour95@gmail.com>
2022-12-13 17:56:00 +0100

    added random sleep time

--- send_email.py
+++ send_email.py
@@ -1,4 +1,4 @@
-import smtplib, ssl
+import smtplib, ssl, time, random
 
 with open("test_email.txt") as f:
     lines = f.readline()
@@ -9,12 +9,14 @@ with open("context.txt", encoding='utf-8') as f:
 
 port = 465 
 smtp_server = "smtp.gmail.com"
-sender_email = "your email address"
-password = "generate your own app password"
+sender_email = "your email"
+password = "generate email token with your gmail"
 message = context.encode("utf-8")
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password)
     for receiver_email in test_email:
+        ran_num = random.randint(180,300)
+        time.sleep(ran_num)
         server.sendmail(sender_email, receiver_email, message)
